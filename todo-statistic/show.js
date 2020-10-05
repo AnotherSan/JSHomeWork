26c4d9206c143c1aa62f6a85c546415396e88c69
AnotherSan <43879526+AnotherSan@users.noreply.github.com>
2020-10-05 19:48:53 +0500

    ShowTableFix

--- todo-statistic/show.js
+++ todo-statistic/show.js
@@ -14,27 +14,46 @@ function show (files) {
     let name=' ';
     let date=' ';
     let comment=' ';
+    //let maxWidthName=0;
+    //let maxWidthDate=0;
+    //let maxWidthComment=0;
     for(let i=0;i<result.length;i++){
-        let tableResult=result[i].replace('//'+ 'TODO','').split(';');
-        if(tableResult.length<2){
+        let tableResult=result[i].replace('//'+' TODO','').split(';');
+        if(tableResult.length<2){//Случай когда нет имени и даты
             comment=tableResult;
-            res.push(`' '\t | ' '\t | ${comment}\t`)
+            res.push(' |'+" ".repeat(10)+' |'+" ".repeat(10)+'  |'+comment)
         }
         else{
         for(let j=0;j<tableResult.length;j++){
                 if(tableResult[0]===undefined){name=''}else{name=tableResult[0];}
-                if(tableResult[j-1]===undefined){date=''}else{date=tableResult[j-1];}
-                if(tableResult[j]===undefined){comment=''}else{comment=tableResult[j];}
-                var resStr=`${name}\t | ${date}\t | ${comment}\t`;
+                if(tableResult[1]===undefined){date=''}else{date=tableResult[1];}
+                if(tableResult[2]===undefined){comment=''}else{comment=tableResult[2];}
+                //Тут идет подбор максимальной строки, можно еще через, можно через массивы и sort, но я не доперла 
+                //if(tableResult[0].length>maxWidthName){
+                  //  maxWidthName=tableResult[0].length;
+                //}
+                //if(tableResult[1].length>maxWidthDate){          
+                //  maxWidthDate=tableResult[1].length;           
+                //}
+                //if(tableResult[2].length>maxWidthComment){
+                  //  maxWidthComment=tableResult[2].length;
+                //}
+                if(name.length<1&&date.length<1){
+                    res.push('| '+" ".repeat(10)+' |'+" ".repeat(10)+' |'+" ".repeat(10)+' |');
+                }
+                var resultName = name.slice(0, 10) + (name.length > 10 ? "" : " ".repeat(10-name.length));
+                //var resultDate = date.slice(0, 10) + (date.length > 10 ? "" : " ".repeat(10-date.length));
+                //var resultComment = comment.slice(0, 50) + (comment.length > 50 ? "" : " ".repeat(50-comment.length));
         }
-        res.push(resStr);
+        res.push('| '+resultName+' |'+date+' |'+comment+' |');
     }
 }
-
-    for(let el=0;el<res.length;el++){
+    
+for(let el=0;el<res.length;el++){
         console.log(res[el]);
     }
 }
+
 module.exports = {
     show
 };
